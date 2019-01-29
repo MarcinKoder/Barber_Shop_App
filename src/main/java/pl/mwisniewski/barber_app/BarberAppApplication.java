fc18457b3e47f05e6154c4274d65db352454bcf3
MarcinKoder <wisniewski.marcin17@gmail.com>
2019-01-29 10:25:22 +0100

    SpringBoot configuration added

---
+++ src/main/java/pl/mwisniewski/barber_app/BarberAppApplication.java
@@ -0,0 +1,14 @@
+package pl.mwisniewski.barber_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberAppApplication.class, args);
+	}
+
+}
+
